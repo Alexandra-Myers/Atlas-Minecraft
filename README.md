5223c9eead13a60deaa2017810b423c297f03fd6
zOnlyKroks <finnrades@gmail.com>
2022-08-20 10:48:57 +0200

    Commit all the shithole being gradle

--- README.md
+++
@@ -1,3 +0,0 @@
-# Atlas-Minecraft
-
-An open source edition of Minecraft built from the ground up for an optimized Java Experience with mod support built in.
---
+++ src/main/java/net/atlas/minecraft/server/MinecraftServer.java
@@ -0,0 +1,47 @@
+package net.atlas.minecraft.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MinecraftServer {
+
+    private static int port = 35566;
+
+    public static void main(String[] args) {
+        try{
+            run();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch)
+                                throws Exception {
+                        }
+                    }).option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+}
