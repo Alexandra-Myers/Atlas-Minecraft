73a114bc9965db5747877913b717b653c9180791
Alexandra-Myers <bia416cat@gmail.com>
2022-08-20 20:09:50 -0400

    rudimentary registry

--- src/main/java/net/atlas/minecraft/common/registry/RegistryKey.java
+++ src/main/java/net/atlas/minecraft/common/registry/RegistryKey.java
@@ -8,11 +8,11 @@ public class RegistryKey<T> extends ArrayList<T> {
 
     }
     public void add(String name, int index, T element) {
-        ID = new Identifier(name, index, element.getClass());
+        ID = new Identifier<T>(name, index, (T) element.getClass());
         super.add(index, element);
     }
     public void add(String namespace, String path, int index, T element) {
-        ID = new Identifier(namespace, path, index, element.getClass());
+        ID = new Identifier<T>(namespace, path, index, (T) element.getClass());
         super.add(index, element);
     }
     public Identifier getID() {
