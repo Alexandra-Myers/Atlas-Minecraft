0a5043ace2f8c1654e7a92a3836d9d2aa258122c
Alexandra-Myers <bia416cat@gmail.com>
2022-08-21 10:16:01 -0400

    added another test item

--- src/main/java/net/atlas/minecraft/common/registry/Registries.java
+++ src/main/java/net/atlas/minecraft/common/registry/Registries.java
@@ -10,7 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Registries {
-    public static RegistryKey<Item> itemRegistry = new RegistryKey<>(40);
+    public static RegistryKey<Item> itemRegistry = new RegistryKey<>(100);
     public Registries() throws Exception {
         initializeObject(this);
         init(Logger.getGlobal());
